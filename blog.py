7a9790079f0f242a1ffc9572d19881c0232e22af
Vitaly <vitalykk1983@gmail.com>
2019-12-19 20:38:02 +0200

    Add login page

--- blog.py
+++ blog.py
@@ -20,7 +20,7 @@ posts = [
 ]
 
 @app.route('/')
-def index():
+def home():
     return render_template('home.html', posts=posts)
 
 
@@ -34,12 +34,18 @@ def register():
     form = RegistrationFrom()
     if form.validate_on_submit():
         flash('Account created for {}!'.format(form.username.data), 'success')
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginFrom()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'pass':
+            flash('You logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('login unseccess', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
