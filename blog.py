b1de80c64f6ad005477e7d7d316daad0d7373df5
Vitaly <vitalykk1983@gmail.com>
2019-12-19 12:43:14 +0200

    Add new line for all python's file

--- blog.py
+++ blog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationFrom, LoginFrom
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '435klf44l60sqqer'
 
 posts = [
     {
