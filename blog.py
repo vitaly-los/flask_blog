4fddbefc6bd4815fbbee1110fbd2139f9cf3065e
Vitaly <vitalykk1983@gmail.com>
2019-12-22 23:03:09 +0200

    Add column for user and post

--- blog.py
+++ blog.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationFrom, LoginFrom
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '435klf44l60sqqer'
@@ -16,15 +18,26 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True,  nullable=False)
-    email = db.Column(db.String(120), unique=True,  nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20),
                            nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return 'User({}, {}, {})' \
-            .format(self.username, self.email, self.image_file)
+        return 'User({}, {}, {})'.format(self.username, self.email, self.image_file)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.Datetime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return 'Post({}, {}, {})'.format(self.title, self.email, self.date_posted)
 
 
 posts = [
