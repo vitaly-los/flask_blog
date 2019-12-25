aa6e8d880ee09ce6e27cc074e15c3f2a17d5043e
Vitaly <vitalykk1983@gmail.com>
2019-12-25 19:45:13 +0200

    Update doc.md how  support python3.6 in venv

--- blog.py
+++ blog.py
@@ -26,18 +26,18 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return 'User({}, {}, {})'.format(self.username, self.email, self.image_file)
+        return "User('{}', '{}', '{}')".format(self.username, self.email, self.image_file)
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.Datetime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return 'Post({}, {}, {})'.format(self.title, self.email, self.date_posted)
+        return "Post('{}', '{}')".format(self.title, self.date_posted)
 
 
 posts = [
--- doc.md
+++ doc.md
@@ -1,12 +1,28 @@
 python3 -m venv venv        *Create a virtual envoriment in python 3*
 
+For python3.6
+ 
+Step 1) Make a Virtual Environment with Python 3.6 ...
+
+*python3.6 -m venv env --without-pip*
+
+Step 2) Activate your virtual environemnt ...
+
+*source env/bin/activate*
+
+Step 3) Install pip into your environemnt ...
+
+*curl https://bootstrap.pypa.io/get-pip.py | python3*
+
 >for development flask run on <http://127.0.0.1:5000>
+>
 >>source venv/bin/activate    *Activate virtual envoriment* 
 
 >>python3  index.py           *Run app*
 Pylint give error for db.Model. Switch to flake8
 
 Install flask-wtf for working with forms.
+
 Flask-wtf  *Simple integration of Flask and WTForms, including CSRF, file upload, and reCAPTCHA.*
 
 Install flask-sqlalchemy for working with database
